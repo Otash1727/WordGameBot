a064d326c35abc70a003f52385c34f24fca2b5d4
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2024-02-10 07:49:18 +0300

    new fix

--- BotScripts/handlers/BotAdmin.py
+++ BotScripts/handlers/BotAdmin.py
@@ -1,11 +1,22 @@
 from BotScripts.create_bot import bot,dp 
 from aiogram import Router,F
 from aiogram.filters import Command
-from aiogram.types import Message,BotCommand,BotCommandScopeChat
+from aiogram.types import Message,BotCommand,BotCommandScopeChat,InlineKeyboardButton,CallbackQuery
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters import BaseFilter
+from wordgame.models import GamersList,MatchList,ChempionsList
+ 
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
+    
+
 
 router=Router()
 chat_id=None
+
 
 @router.message(Command('start'))
 async def runbot(message:Message):
@@ -20,15 +31,34 @@ async def help(message:Message):
     await bot.send_message(chat_id=message.from_user.id,text="If you want to use the bot,add the bot to your group and give admin and send /new_match")
     await bot.set_my_commands([BotCommand(command='new_macht1',description='Create a new match',)],BotCommandScopeChat(chat_id=message.from_user.id),request_timeout=2)
 
-@router.message(F.)
-async def check_status(message:Message):
-    global chat_id
+
+@router.message(Command('new_match'))
+async def fff(message:Message):
     chat_id=message.chat.id
-    bot_status= await bot.get_chat_member(chat_id=chat_id,user_id=bot.id)
-    print(bot_status.status)
+    check_status= await bot.get_chat_member(chat_id=chat_id,user_id=bot.id)
+    join_kb=InlineKeyboardBuilder()
+    join_kb.row(InlineKeyboardButton(text='Join',callback_data='join'))
+    print(check_status.status)  
+    if check_status.status=='administrator':
+        await bot.send_message(chat_id=chat_id,text="Great, get ready and click on start button below",reply_markup=join_kb.as_markup())        
+    else:
+        await bot.send_message(chat_id=chat_id,text='You can play only with an opponent(s) and in a group.')
+        await bot.send_message(chat_id=chat_id,text="You can play only with an opponent(or more) and in a group.")
+    
+@router.callback_query(F.data=='join')
+async def join_game(callback:CallbackQuery):  
+    chat_id=callback.message.chat.id
+    channel_info=await bot.get_chat(chat_id=chat_id)
+    await callback.answer('You are already practicipating in the game!\n Please wait for others to join',show_alert=True)
+    print(channel_info.title)
+    fsm_save=MatchList(channel_name=channel_info.title,channel_ID=channel_info.id)
+    fsm_save.save()
+
+@router.message()
+async def empty_handler(message:Message):
     await bot.set_my_commands([BotCommand(command='new_match',description='Star new match')],BotCommandScopeChat(chat_id=chat_id))
-
-
+    
+    
 
     
 
--- wordgame/models.py
+++ wordgame/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+from django.db.models.functions import Now
 
-# Create your models here.
+
+class MatchList(models.Model):
+    channel_name=models.CharField(max_length=200)
+    channel_ID=models.IntegerField()  
+    match_ID=models.AutoField(primary_key=True)
+    players_count=models.IntegerField(null=True,blank=True)
+    word_count=models.IntegerField(null=True,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True,db_index=True)
+    DisplyFields=['match_ID','channel_name','channel_ID']
+    SearchFilds=['channel_ID','channel_name','match_ID','players_count','word_count']
+    FiltersFields=['channel_name']
+
+class GamersList(models.Model):
+    match_ID=models.IntegerField(null=True,blank=True)
+    user_id=models.IntegerField()
+    user_name=models.CharField(max_length=200)
+    found_word_count=models.IntegerField(null=True,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True,db_index=True)
+    Match_List=models.ForeignKey(to=MatchList,on_delete=models.CASCADE,null=True,blank=True)  
+    DisplayFields=['match_ID','user_name']
+    SearchFields=['match_ID','user_id','user_name','found_word_count']
+    FiltersFields=['found_word_count']
+
+class ChempionsList(models.Model):
+    match_ID=models.IntegerField(null=True,blank=True)
+    channel_name=models.CharField(max_length=200)
+    channel_ID=models.IntegerField()
+    user_id=models.IntegerField()
+    user_name=models.CharField(max_length=100)
+    results=models.IntegerField()
+    MatchList=models.ForeignKey(to=MatchList,on_delete=models.CASCADE,null=True,blank=True)  
+    DisplayFields=['match_ID','user_name']
+    SearchFields=['match_ID','channel_ID','channel_name','user_id','user_name','results']
+    FiltersFields=['channel_name']
+    
+
+
+
+
+
+
+
+
